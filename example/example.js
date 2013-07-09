f397c72112caee8dd12fc20f94575fa418250273
Mikola Lysenko <mikolalysenko@gmail.com>
2013-07-09 12:42:48 -0500

    built with wrong version

--- example/example.js
+++ example/example.js
@@ -1,26 +1,37 @@
-"use strict"
-
 var shell = require("gl-now")()
+var camera = require("game-shell-orbit-camera")(shell)
 var createTileMap = require("gl-tile-map")
 var createBuffer = require("gl-buffer")
 var createVAO = require("gl-vao")
-var glm = require("gl-matrix")
 var ndarray = require("ndarray")
 var fill = require("ndarray-fill")
 var ops = require("ndarray-ops")
 var terrain = require("isabella-texture-pack")
 var createAOMesh = require("ao-mesher")
 var createAOShader = require("../aoshader.js")
+var glm = require("gl-matrix")
 var mat4 = glm.mat4
 
-//The shader
-var shader
+//WebGL state
+var shader, texture, vao, vertexCount
 
-//Tile texture
-var texture
+//Voxel resolution
+var resolution = [32,32,32]
 
-//Mesh data variables
-var vao, vertexCount
+//Modify this to generate different terrains:
+function generateVoxels(i,j,k) {
+  var x = i - 16
+  var y = j - 16
+  var z = k - 16
+  if(x*x+y*y+z*z < 30) {
+    if(k < 16) {
+      return 1<<15
+    }
+    return (1<<15)+1
+  }  
+  return 0
+}
+
 
 shell.on("gl-init", function() {
   var gl = shell.gl
@@ -29,27 +40,19 @@ shell.on("gl-init", function() {
   shader = createAOShader(gl)
   
   //Create some voxels
-  var voxels = ndarray(new Uint16Array(32*32*32), [32,32,32])
-  voxels.set(16,16,16, 1<<15)
+  var voxels = ndarray(new Int32Array(resolution[0]*resolution[1]*resolution[2]),
+                      resolution)
+  fill(voxels, generateVoxels)
   
-  fill(voxels, function(i,j,k) {
-    var x = Math.abs(i - 16)
-    var y = Math.abs(j - 16)
-    var z = Math.abs(k - 16)
-    if(x*x+y*y+z*z < 30) {
-      if(k < 16) {
-        return 1<<15
-      }
-      return (1<<15)+1
-    }
-    return 0
-  })
+  //Set up camera
+  var c = [resolution[0]>>>1, resolution[1]>>>1, resolution[2]>>>1]
+  camera.lookAt([c[0], c[1], c[2]+2*resolution[2]], c, [0,1,0])
   
   //Compute mesh
   var vert_data = createAOMesh(voxels)
   
   //Convert mesh to WebGL buffer
-  vertexCount = Math.floor(vert_data.length/8)
+  vertexCount = vert_data.length>>3
   var vert_buf = createBuffer(gl, vert_data)
   vao = createVAO(gl, undefined, [
     { "buffer": vert_buf,
@@ -68,6 +71,7 @@ shell.on("gl-init", function() {
     }
   ])
   
+  //Load texture map
   var tiles = ndarray(terrain.data,
     [16,16,terrain.shape[0]>>4,terrain.shape[1]>>4,4],
     [terrain.stride[0]*16, terrain.stride[1]*16, terrain.stride[0], terrain.stride[1], terrain.stride[2]], 0)
@@ -77,35 +81,29 @@ shell.on("gl-init", function() {
 
 shell.on("gl-render", function(t) {
   var gl = shell.gl
-
+  var A = mat4.create()
+  
+  //Set WebGL parameters
   gl.enable(gl.CULL_FACE)
   gl.enable(gl.DEPTH_TEST)
 
   //Bind the shader
   shader.bind()
   
-  //Set shader attributes
+  //Set shader attributes and uniforms
   shader.attributes.attrib0.location = 0
   shader.attributes.attrib1.location = 1
-  
-  //Set up camera parameters
-  var A = new Float32Array(16)
-  shader.uniforms.projection = mat4.perspective(A, Math.PI/4.0, shell.width/shell.height, 1.0, 1000.0)
-  
-  var t = 0.0001*Date.now()
-  
-  shader.uniforms.view = mat4.lookAt(A, [30*Math.cos(t) + 16,20,30*Math.sin(t)+16], [16,16,16], [0, 1, 0])
-
-  //Set tile size
+  shader.uniforms.projection = mat4.perspective(A, 
+                                                Math.PI/4.0, 
+                                                shell.width/shell.height, 
+                                                1.0, 
+                                                1000.0)
+  shader.uniforms.view = camera.view()
+	shader.uniforms.model = mat4.identity(A)
+  shader.uniforms.tileMap = texture.bind()
   shader.uniforms.tileSize = 16.0
   
-  //Set texture
-  if(texture) {
-    shader.uniforms.tileMap = texture.bind()
-  }
-  
-  //Draw instanced mesh
-  shader.uniforms.model = mat4.identity(A)
+  //Draw object
   vao.bind()
   gl.drawArrays(gl.TRIANGLES, 0, vertexCount)
   vao.unbind()
